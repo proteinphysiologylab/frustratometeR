efa87160f3883397d558242a4f00d3aff422fe06
gonzaparra <parra.gonzalo@gmail.com>
2020-08-26 04:25:51 +0200

    Update PdbCoords2Lammps.sh

--- inst/Scripts/AWSEMFiles/AWSEMTools/PdbCoords2Lammps.sh
+++ inst/Scripts/AWSEMFiles/AWSEMTools/PdbCoords2Lammps.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-#export s=$HOME/opt/script
+export s=$HOME/opt/script
+
+if [[ ! $# -eq 2 ]]
+then
+        echo
+        echo "> $0 pdb_id project_name"
+        echo
+        exit
+fi
 
 pdb_file=$1
 output_file=$2
@@ -8,5 +16,5 @@ output_file=$2
 echo $pdb_file
 echo $output_file
 
-python3 $3/AWSEMFiles/AWSEMTools/PDBToCoordinates.py $pdb_file $output_file".coord"
-python3 $3/AWSEMFiles/AWSEMTools/CoordinatesToWorkLammpsDataFile.py $3 $output_file".coord" "data."$output_file -b
+python3 $s/PDBToCoordinates.py $pdb_file $output_file".coord"
+python3 $s/CoordinatesToWorkLammpsDataFile.py $output_file".coord" "data."$output_file -b
