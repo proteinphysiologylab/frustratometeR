352990abd7fb1b0622cad1e4ccb8b37a3e9e8f30
gonzaparra <parra.gonzalo@gmail.com>
2021-02-03 21:14:43 +0100

    Update frustra.sh

--- Examples/frustra.sh
+++ Examples/frustra.sh
@@ -2,16 +2,16 @@
 echo "FrustratometeR"
 
 if [ "configurational" = "$2" ]; then
-    docker run -v $1:/pdb -it cesarleonetti/frustratometer:version1 R -e "library(frustratometeR)" -e "dir_frustration(PdbsDir = '/pdb/', Mode = '$2', ResultsDir = '/pdb/')"
+    docker run -v $1:/pdb -it lfp/frustratometer:version1 R -e "library(frustratometeR)" -e "dir_frustration(PdbsDir = '/pdb/', Mode = '$2', ResultsDir = '/pdb/')"
     exit
 fi
 
 if [ "mutational" = "$1" ]; then
-    docker run -v $1:/pdb -it cesarleonetti/frustratometer:version1 R -e "library(frustratometeR)" -e "dir_frustration(PdbsDir = '/pdb/', Mode = '$2', ResultsDir = '/pdb/')"
+    docker run -v $1:/pdb -it lfp/frustratometer:version1 R -e "library(frustratometeR)" -e "dir_frustration(PdbsDir = '/pdb/', Mode = '$2', ResultsDir = '/pdb/')"
     exit
 fi
 
 if [ "singleresidue" = "$1" ]; then
-    docker run -v $1:/pdb -it cesarleonetti/frustratometer:version1 R -e "library(frustratometeR)" -e "dir_frustration(PdbsDir = '/pdb/', Mode = '$2', ResultsDir = '/pdb/')"
+    docker run -v $1:/pdb -it lfp/frustratometer:version1 R -e "library(frustratometeR)" -e "dir_frustration(PdbsDir = '/pdb/', Mode = '$2', ResultsDir = '/pdb/')"
     exit
 fi
