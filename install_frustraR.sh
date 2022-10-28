9e8aa6c7ab7c991b1a015a0257ab45b81d9187d3
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2022-10-28 19:07:21 -0300

    Update install_frustraR.sh

--- install_frustraR.sh
+++ install_frustraR.sh
@@ -14,10 +14,28 @@ apt install wget mc r-base python3 python3-pip pymol libmagick++-dev libcurl4-op
 apt install libcurl4-gnutls-dev libxml2-dev -y
 python3 -m pip install numpy biopython leidenalg
 apt install software-properties-common -y
+apt install libharfbuzz-dev libfribidi-dev
+apt install libudunits2-dev
+apt install libgeos-dev
+apt-get install libgdal-dev
+apt install cmake
+apt-get install cargo
+apt-get install -y libpoppler-cpp-dev
+apt-get install -y libavfilter-dev
+apt install libtesseract-dev libleptonica-dev
+apt-get install tesseract-ocr-eng
 
 Rscript -e "update.packages(ask=FALSE, checkBuilt=TRUE)"
 Rscript -e "install.packages('usethis', dependencies = TRUE)"
 Rscript -e "install.packages('devtools', dependencies = TRUE)"
-Rscript -e "install.packages(c('ggplot2','ggrepel','dplyr','igraph','FactoMineR','Hmisc','msa','magick','leiden'))"
+Rscript -e "install.packages('ggplot2', dependencies = TRUE)"
+Rscript -e "install.packages('ggrepel', dependencies = TRUE)"
+Rscript -e "install.packages('dplyr', dependencies = TRUE)"
+Rscript -e "install.packages('igraph', dependencies = TRUE)"
+Rscript -e "install.packages('FactoMineR', dependencies = TRUE)"
+Rscript -e "install.packages('Hmisc', dependencies = TRUE)"
+Rscript -e "install.packages('magick', dependencies = TRUE)"
+Rscript -e "install.packages('leiden', dependencies = TRUE)"
 Rscript -e "devtools::install_github('proteinphysiologylab/frustratometeR')"
+
 
