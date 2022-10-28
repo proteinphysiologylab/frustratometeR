3103883230f0451dd0e1513c555fcbb7bd77d4d6
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2022-10-28 16:47:22 -0300

    Add files via upload

---
+++ install_frustraR.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#FROM ubuntu:22.04
+
+#MAINTAINER Freiberger Maria Ines
+#For R installation
+
+apt-get update
+apt install -y --no-install-recommends software-properties-common dirmngr
+apt-get install wget
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt install -y r-base r-base-core r-recommended r-base-dev
+
+#For frustraR package installation
+
+apt update -y
+apt install wget mc r-base python3 python3-pip pymol libmagick++-dev libcurl4-openssl-dev libssl-dev libgit2-dev -y
+apt install libcurl4-gnutls-dev libxml2-dev -y
+python3 -m pip install numpy biopython leidenalg
+apt install software-properties-common -y
+
+Rscript -e "update.packages(ask=FALSE, checkBuilt=TRUE)"
+Rscript -e "install.packages('usethis', dependencies = TRUE)"
+Rscript -e "install.packages('devtools', dependencies = TRUE)"
+Rscript -e "install.packages(c('ggplot2','ggrepel','dplyr','igraph','FactoMineR','Hmisc','msa','magick','leiden'))"
+Rscript -e "devtools::install_github('proteinphysiologylab/frustratometeR')"
+
