27095103971d9a2ee31888d6589f524449dc8258
Gonzalo Parra <parra.gonzalo@gmail.com>
2024-10-17 17:40:15 +0200

    Update functions.R
    Fixed KCX change to standard LYS

--- R/functions.R
+++ R/functions.R
@@ -364,6 +364,10 @@ calculate_frustration <- function(PdbFile = NULL, PdbID = NULL, Chain = NULL, El
   Pdb$atom$type[which(Pdb$atom$resid == "CY1")] <- "ATOM"
   Pdb$atom$resid[which(Pdb$atom$resid == "CY1")] <- "CYS"
 
+    #KCX to LYS filter
+  Pdb$atom$type[which(Pdb$atom$resid == "KCX")] <- "ATOM"
+  Pdb$atom$resid[which(Pdb$atom$resid == "KCX")] <- "LYS"
+
   # Here we filter remove non-protein atoms.
   #The Protein atom from bio3d::aa.table$aa3
   #"ALA" "ARG" "ASN" "ASP" "CYS" "GLN" "GLU" "GLY" "HIS" "ILE" "LEU" "LYS" "MET" "PHE"
